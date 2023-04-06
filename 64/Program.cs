775dc7069dc8a0441d0f9306567917473ce30f7d
Viktoria <vikusya25072000@gmail.com>
2023-04-06 17:38:54 +0300

    решила первую задачу

---
+++ 64/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void OutPutNamber (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    OutPutNamber (number - 1);
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+OutPutNamber(number);
